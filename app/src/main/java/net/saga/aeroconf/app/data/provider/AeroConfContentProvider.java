5b26da94291ddb5da11b27f39f15ca9880688c2d
Summers Pittman <secondsun@gmail.com>
2014-04-29 11:56:06 -0400

    initial impl of Content Provider

--- app/src/main/java/net/saga/aeroconf/app/data/provider/AeroConfContentProvider.java
+++ app/src/main/java/net/saga/aeroconf/app/data/provider/AeroConfContentProvider.java
@@ -6,8 +6,12 @@ import android.database.Cursor;
 import android.net.Uri;
 
 import net.saga.aeroconf.app.data.provider.contract.ConfContract;
-import net.saga.aeroconf.app.data.provider.operations.Operation;
+import net.saga.aeroconf.app.data.provider.operations.BulkInsertOp;
 import net.saga.aeroconf.app.data.provider.operations.DeleteOp;
+import net.saga.aeroconf.app.data.provider.operations.InsertOp;
+import net.saga.aeroconf.app.data.provider.operations.Operation;
+import net.saga.aeroconf.app.data.provider.operations.QueryOp;
+import net.saga.aeroconf.app.data.provider.operations.UpdateOp;
 import net.saga.aeroconf.app.data.vo.Presentation;
 import net.saga.aeroconf.app.data.vo.Room;
 import net.saga.aeroconf.app.data.vo.Schedule;
@@ -42,10 +46,16 @@ public class AeroConfContentProvider extends AbstractAeroConfProvider implements
 
         switch (match) {
             case RoomContract.ROOM:
+                op = new DeleteOp(resolver, RoomContract.URI);
+                break;
             case SpeakerContract.SPEAKER:
+                op = new DeleteOp(resolver, SpeakerContract.URI);
+                break;
             case PresentationContract.PRESENTATION:
+                op = new DeleteOp(resolver, PresentationContract.URI);
+                break;
             case ScheduleContract.SCHEDULE:
-                op = new DeleteOp(resolver, uri);
+                op = new DeleteOp(resolver, ScheduleContract.URI);
                 break;
         }
 
@@ -81,19 +91,26 @@ public class AeroConfContentProvider extends AbstractAeroConfProvider implements
     public Uri insert(Uri uri, ContentValues values) {
 
         int match = MATCHER.match(uri);
-
+        Operation<Uri> operation;
         switch (match) {
             case RoomContract.ROOM:
+                operation = new InsertOp<>(resolver, RoomContract.URI, Room.class);
                 break;
             case SpeakerContract.SPEAKER:
+                operation = new InsertOp<>(resolver, SpeakerContract.URI, Speaker.class);
                 break;
             case PresentationContract.PRESENTATION:
+                operation = new InsertOp<>(resolver, PresentationContract.URI, Presentation.class);
                 break;
             case ScheduleContract.SCHEDULE:
+                operation = new InsertOp<>(resolver, ScheduleContract.URI, Schedule.class);
                 break;
+            default: {
+                throw new IllegalArgumentException(String.format("%s not supported", uri.toString()));
+            }
         }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        return execute(uri, new ContentValues[]{values}, null, null, operation);
     }
 
     @Override
@@ -104,38 +121,81 @@ public class AeroConfContentProvider extends AbstractAeroConfProvider implements
 
         switch (match) {
             case RoomContract.ROOM:
-                break;
             case SpeakerContract.SPEAKER:
-                break;
             case PresentationContract.PRESENTATION:
-                break;
             case ScheduleContract.SCHEDULE:
-                break;
+                return execute(uri, null, null, null, new QueryOp());
+            default:
+                throw new IllegalArgumentException(String.format("%s not supported", uri.toString()));
         }
 
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
-
+        Operation<Integer> op;
         int match = MATCHER.match(uri);
+        ContentValues[] vals;
+
+        if (values == null) {
+            vals = new ContentValues[]{null};
+        } else {
+            vals = new ContentValues[]{values};
+        }
 
         switch (match) {
             case RoomContract.ROOM:
+                op = new UpdateOp<>(resolver, RoomContract.URI, Room.class);
                 break;
             case SpeakerContract.SPEAKER:
+                op = new UpdateOp<>(resolver, SpeakerContract.URI, Speaker.class);
                 break;
             case PresentationContract.PRESENTATION:
+                op = new UpdateOp<>(resolver, PresentationContract.URI, Presentation.class);
                 break;
             case ScheduleContract.SCHEDULE:
+                op = new UpdateOp<>(resolver, ScheduleContract.URI, Schedule.class);
                 break;
-
+            default:
+                throw new IllegalArgumentException(String.format("%s not supported", uri.toString()));
         }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        Integer res = execute(uri, vals, selection, selectionArgs, op);
+        if (res == null) {
+            res = 0;
+        }
+        return res;
     }
 
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        int match = MATCHER.match(uri);
+        Operation<Integer> operation;
+        switch (match) {
+            case RoomContract.ROOM:
+                operation = new BulkInsertOp<>(resolver, RoomContract.URI, Room.class);
+                break;
+            case SpeakerContract.SPEAKER:
+                operation = new BulkInsertOp<>(resolver, SpeakerContract.URI, Speaker.class);
+                break;
+            case PresentationContract.PRESENTATION:
+                operation = new BulkInsertOp<>(resolver, PresentationContract.URI, Presentation.class);
+                break;
+            case ScheduleContract.SCHEDULE:
+                operation = new BulkInsertOp<>(resolver, ScheduleContract.URI, Schedule.class);
+                break;
+            default: {
+                throw new IllegalArgumentException(String.format("%s not supported", uri.toString()));
+            }
+        }
 
+        Integer res = execute(uri, values, null, null, operation);
+
+        if (res == null) {
+            return 0;
+        } else {
+            return res;
+        }
+    }
 }
