f50a518025885c034b689e30f236ac25fc0e8ace
Summers Pittman <secondsun@gmail.com>
2014-04-29 11:44:38 -0400

    Update is Generic

---
+++ app/src/main/java/net/saga/aeroconf/app/data/provider/operations/UpdateOp.java
@@ -0,0 +1,41 @@
+package net.saga.aeroconf.app.data.provider.operations;
+
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+
+import com.google.gson.Gson;
+
+import net.saga.aeroconf.app.data.provider.contract.ConfContract;
+
+import org.jboss.aerogear.android.impl.datamanager.SQLStore;
+
+public class UpdateOp<T> implements Operation<Integer> {
+
+    private final ContentResolver resolver;
+    private final Uri notifyUri;
+    private final Class<T> klass;
+
+    public UpdateOp(ContentResolver resolver, Uri notifyUri, Class<T> klass) {
+        this.resolver = resolver;
+        this.notifyUri = notifyUri;
+        this.klass = klass;
+    }
+
+    @Override
+    public Integer exec(Gson gson, SQLStore store, Uri uri, ContentValues[] values, String selection, String[] selectionArgs) {
+        if (selectionArgs == null || selectionArgs[0] == null) {
+            store.reset();
+        } else {
+            Long id = Long.getLong(selectionArgs[0]);
+            store.remove(id);
+        }
+        T object = gson.fromJson(values[0].getAsString(ConfContract.DATA), klass);
+        store.save(object);
+        if (values[0].getAsBoolean(ConfContract.NOTIFY) != null && values[0].getAsBoolean(ConfContract.NOTIFY)) {
+            resolver.notifyChange(notifyUri, null, false);
+        }
+        return 1;
+    }
+}
