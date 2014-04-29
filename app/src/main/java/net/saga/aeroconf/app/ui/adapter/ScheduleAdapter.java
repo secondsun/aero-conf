f090f669fc6a5a1a83eb5cce8a1ffdf19a03ff08
Summers Pittman <secondsun@gmail.com>
2014-04-29 12:18:07 -0400

    Creating schedule fragment

---
+++ app/src/main/java/net/saga/aeroconf/app/ui/adapter/ScheduleAdapter.java
@@ -0,0 +1,216 @@
+package net.saga.aeroconf.app.ui.adapter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import net.saga.aeroconf.app.R;
+import net.saga.aeroconf.app.data.vo.Schedule;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by summers on 4/29/14.
+ */
+public class ScheduleAdapter extends BaseAdapter {
+
+
+    public static final int DATE_TYPE = 0;
+    public static final int ITEM_TYPE = 1;
+    private static final String TAG = ScheduleAdapter.class.getSimpleName();
+
+
+    final DateFormat format = new SimpleDateFormat("h:mm a");
+
+
+    private List<Schedule> calendar;
+
+    private Date dayOne;
+    private Date dayTwo;
+
+    private int dayOneSize = 0;
+    private final Context appContext;
+
+
+    public ScheduleAdapter(List<Schedule> calendar, Context appContext) {
+        Log.d(TAG, "Constructor with data:" + calendar.size());
+
+        this.calendar = calendar;
+        this.calendar = new ArrayList<Schedule>(calendar);
+
+        dayOne = Calendar.getInstance().getTime();
+        dayTwo = Calendar.getInstance().getTime();
+
+        dayOneSize = 0;
+        this.appContext = appContext;
+        for (Schedule item : calendar) {
+            if (item.fromTime.after(dayTwo)) {
+
+            } else {
+                dayOneSize++;
+            }
+        }
+
+
+    }
+
+    public void update(List<Schedule> calendar) {
+        Log.d(TAG, "Updating new data:" + calendar.size());
+        this.calendar = new ArrayList<Schedule>(calendar);
+        dayOne = Calendar.getInstance().getTime();
+        dayTwo = Calendar.getInstance().getTime();
+
+        dayOneSize = 0;
+        for (Schedule item : calendar) {
+            if (item.fromTime.after(dayTwo)) {
+
+            } else {
+                dayOneSize++;
+            }
+        }
+
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                ScheduleAdapter.super.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void zero(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    @Override
+    public int getCount() {
+        return calendar.size() + 2;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (position == 0) {
+            return dayOne;
+        } else if (position == dayOneSize + 1) {
+            return dayTwo;
+        } else if (position > dayOneSize) {
+            return calendar.get(position - 2); //Offset for two date header items
+        } else {
+            return calendar.get(position - 1); //Offset for one date header item
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return DATE_TYPE;
+        } else if (position == dayOneSize + 1) {
+            return DATE_TYPE;
+        } else if (position > dayOneSize) {
+            return ITEM_TYPE;
+        } else {
+            return ITEM_TYPE;
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        switch (getItemViewType(position)) {
+            case DATE_TYPE:
+                convertView = getDateView(convertView);
+                ((ViewHolder) convertView.getTag()).date.setText(getDateHeader(position));
+                return convertView;
+            case ITEM_TYPE:
+                convertView = getItemView(convertView);
+
+                ViewHolder holder = (ViewHolder) convertView.getTag();
+                Schedule item = ((Schedule) getItem(position));
+
+                holder.date.setText(format.format(item.fromTime));
+
+                if (item != null) {
+                    holder.date.setBackgroundResource(R.color.abc_search_url_text_holo);
+                    if (item.room_id != null)
+                        holder.roomName.setText("Link room and schedule");
+                    if (item.presentation_id != null) {
+                        holder.title.setText("Link presentation and schedule");
+                    } else {
+                        holder.title.setText(item.title);
+                    }
+                }
+
+                holder.title.setSelected(true);
+                convertView.invalidate();
+                convertView.requestLayout();
+                convertView.refreshDrawableState();
+
+                return convertView;
+        }
+        return null;
+    }
+
+    private String getDateHeader(int position) {
+        return position == 0 ? "Day 0" : "Day 1";
+    }
+
+    private View getDateView(View convertView) {
+        if (convertView == null || convertView.findViewById(R.id.date) == null) {
+            Log.d(TAG, "inflating date");
+            LayoutInflater inflater = (LayoutInflater) appContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.date_breaker_list_item, null);
+            ViewHolder holder = new ViewHolder();
+            holder.date = (TextView) convertView.findViewById(R.id.date);
+            convertView.setTag(holder);
+        }
+        return convertView;
+    }
+
+    private View getItemView(View convertView) {
+        if (convertView == null || convertView.findViewById(R.id.session_room) == null) {
+            Log.d(TAG, "inflating item.  Convert was " + convertView);
+            LayoutInflater inflater = (LayoutInflater) appContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.schedule_list_item, null);
+            ViewHolder holder = new ViewHolder();
+            holder.date = ((TextView) convertView.findViewById(R.id.start_time));
+            holder.roomName = ((TextView) convertView.findViewById(R.id.session_room));
+            holder.title = ((TextView) convertView.findViewById(R.id.session_title));
+            convertView.setTag(holder);
+        }
+        return convertView;
+    }
+
+    public List<Schedule> getCalendar() {
+        return calendar;
+    }
+
+    private static class ViewHolder {
+        private TextView date;
+        private TextView title;
+        private TextView roomName;
+    }
+
+}
---
+++ app/src/main/java/net/saga/aeroconf/app/ui/fragment/ScheduleFragment.java
@@ -0,0 +1,162 @@
+package net.saga.aeroconf.app.ui.fragment;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import net.saga.aeroconf.app.data.provider.contract.ConfContract;
+import net.saga.aeroconf.app.data.vo.Schedule;
+import net.saga.aeroconf.app.ui.adapter.ScheduleAdapter;
+import net.saga.aeroconf.app.util.GsonUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by summers on 4/29/14.
+ */
+public class ScheduleFragment extends Fragment {
+
+    private ScheduleAdapter adapter;
+    private ListView view;
+    private ContentResolver resolver;
+
+    private final Observer observer = new Observer(new Handler());
+    private List<Schedule> calendar = new ArrayList<Schedule>();
+    private AsyncTask<Void, Void, List<Schedule>> calendarLoaderTask;
+
+    public ScheduleFragment() {
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (adapter == null) {
+            adapter = new ScheduleAdapter(calendar, activity.getApplicationContext());
+        }
+        resolver = getActivity().getContentResolver();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = new ListView(inflater.getContext());
+        view.setAdapter(adapter);
+
+        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (adapter.getItem(position) instanceof Schedule) {
+                    Schedule item = (Schedule) adapter.getItem(position);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        resolver.registerContentObserver(ConfContract.ScheduleContract.URI, false, observer);
+
+        calendarLoaderTask = new AsyncTask<Void, Void, List<Schedule>>() {
+            @Override
+            protected synchronized List<Schedule> doInBackground(Void... params) {
+
+                List<Schedule> toReturn = new ArrayList<Schedule>();
+                Cursor cursor = null;
+                try {
+                    cursor = getActivity().getContentResolver().query(ConfContract.ScheduleContract.URI, null, null, null, null);
+
+                    while (cursor != null && cursor.moveToNext()) {
+                        toReturn.add(GsonUtils.GSON.fromJson(cursor.getString(0), Schedule.class));
+                    }
+
+                    return toReturn;
+                } finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+            }
+
+            @Override
+            protected synchronized void onPostExecute(List<Schedule> userCalendars) {
+                if (!super.isCancelled()) {
+                    dataUpdated(userCalendars);
+                }
+            }
+        };
+
+        if (calendar == null || calendar.size() < 3) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+                calendarLoaderTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            else
+                calendarLoaderTask.execute();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        calendarLoaderTask.cancel(false);
+    }
+
+    public synchronized void dataUpdated(Collection<Schedule> newData) {
+        calendar.clear();
+        calendar.addAll(newData);
+        adapter.update(new ArrayList<Schedule>(newData));
+        adapter.notifyDataSetChanged();
+    }
+
+    private final class Observer extends ContentObserver {
+        public Observer(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            AsyncTask<Void, Void, ArrayList<Schedule>> task = new AsyncTask<Void, Void, ArrayList<Schedule>>() {
+                @Override
+                protected ArrayList<Schedule> doInBackground(Void... params) {
+                    Cursor cursor = null;
+                    try {
+                        cursor = resolver.query(ConfContract.ScheduleContract.URI, null, null, null, null);
+                        ArrayList<Schedule> calendar = new ArrayList<>(cursor.getCount());
+                        while (cursor != null && cursor.moveToNext()) {
+                            calendar.add(GsonUtils.GSON.fromJson(cursor.getString(0), Schedule.class));
+                        }
+                        return calendar;
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                    }
+                }
+
+                @Override
+                protected void onPostExecute(ArrayList<Schedule> userCalendars) {
+                    dataUpdated(userCalendars);
+                }
+            };
+        }
+    }
+
+}
